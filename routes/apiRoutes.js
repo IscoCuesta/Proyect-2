836eccefaa13263481a33ac74e3788067018cecd
Magpie <edchavez821@gmail.com>
2019-06-22 14:25:55 -0500

    Added api route for DB

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,21 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/places", function(req, res) {
+    db.Places.findAll({}).then(function(result) {
+      res.json(result);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/places", function(req, res) {
+    db.Places.create(req.body).then(function(result) {
+      res.json(result);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
+  app.delete("/api/places/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
