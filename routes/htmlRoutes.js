c9d963e63a9bb1084544053d0096b95849d48156
Isco <Iscocuesta@hotmail.com>
2019-06-21 21:23:27 -0500

    dot env and route map/:place working fine

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,6 @@
 var db = require("../models");
+const keys = require("../keys");
+var axios = require('axios');
 
 module.exports = function(app) {
   // Load index page
@@ -12,12 +14,33 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  app.get("/maps/:place/:type?", function(req, res) {
+    var APIKEY = process.env.mapbox_id;
+    // var APIKEY = "pk.eyJ1IjoiaXNjb2N1ZXN0YSIsImEiOiJjanV4Y245YmwwbHJiM3lsNmhyeHc1MmV2In0.BsdcljLRiFE9seVq7JuZrQ";
+    var searchPlace = req.params.place;
+    console.log(req.params);
+    if (req.params.type !== null){
+      var searchType =  {where: { type: req.params.type }} ;
+    }else{
+      var searchType = {};
+    }
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+searchPlace+".json?proximity=-99.1228881,19.426?address="+searchPlace+"&access_token="+APIKEY;
+    console.log("querry ajax",queryURL);
+    axios.get(queryURL).then(function(response) {
+
+        // db.Example.findAll({}).then(function(dbExample) {
+        //   res.render("example", {
+        //     example: dbExample
+        //   });
+        // });
+        console.log(response.data);
+        res.json(response.data.features[0].center);
+
+
+  
+
     });
+
   });
 
   // Render 404 page for any unmatched routes
