563f5e50bed19a41887f15fa7f05195b929023e2
Isco <Iscocuesta@hotmail.com>
2019-06-26 19:51:18 -0500

    window location replace

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -105,6 +105,72 @@ module.exports = function(app) {
 
   });
 
+  // Load example page and pass in an example by id
+  app.get("/maps/location", function(req, res) {
+    var APIKEY = process.env.mapbox_id;
+    var searchPlace = req.params.place;
+
+    var searchType = {};
+    if (req.params.type !== undefined && req.params.type !== "null"){
+      var searchType =  {where: { type: req.params.type },
+      include: [{
+        model: db.Dishes
+      }]} ;
+    }else{
+      var searchType = {
+        include: [{
+          model: db.Dishes
+        }]};
+    }
+    var queryURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+searchPlace+".json?proximity=-99.1228881,19.426?address="+searchPlace+"&access_token="+APIKEY;
+
+    axios.get(queryURL).then(function(response) {
+      var searchLat = response.data.features[0].center[1];
+      var searchLon = response.data.features[0].center[0];
+      console.log(searchType);
+      db.Places.findAll(searchType).then(function(dbExample) {
+        var arrByDist = [];
+
+        for(var i =0; i<dbExample.length; i++){
+          var dist = Math.sqrt((Math.pow((searchLon-dbExample[i].lon),2))+(Math.pow((searchLat-dbExample[i].lat),2)));
+          arrByDist[i] = {
+            dist: dist,
+            place: dbExample[i]
+          };
+        };
+        
+        function compare( a, b ) {
+          if ( a.dist < b.dist ){
+            return -1;
+          }
+          if ( a.dist > b.dist ){
+            return 1;
+          }
+          return 0;
+        }
+        
+        arrByDist.sort( compare );
+
+        var responseArr = [];
+
+
+        for(var i =0; i<arrByDist.length && i<5; i++){
+          responseArr[i] = arrByDist[i].place;
+        };
+        res.render("place", {
+          places: responseArr,
+          lon: searchLon,
+          lat: searchLat,
+          zoom: 15,
+          ubicacion: searchPlace
+        });
+
+      });
+
+    });
+
+  });
+
   app.get("/all", function(req, res) {
     db.Places.findAll({}).then(function(dbExamples) {
       res.render("info", {
